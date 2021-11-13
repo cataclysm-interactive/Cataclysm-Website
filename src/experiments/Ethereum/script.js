9782312ef5f32783b93b2742b3bf6f3f7b00f980
Titan3k <59878895+WahooLabs@users.noreply.github.com>
2021-11-13 15:48:14 -0700

    Updated ETH Exper... and Main page

--- src/experiments/Ethereum/script.js
+++ src/experiments/Ethereum/script.js
@@ -1,5 +1,6 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
+const installation = document.querySelector('.installation');
 
 ethereumButton.addEventListener('click', () => {
   getAccount();
@@ -9,4 +10,12 @@ async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
   const account = accounts[0];
   showAccount.innerHTML = account;
+}
+
+if(ethereum == 'undefined')
+{
+  installation.innerHTML = 'No'
+}
+else{
+  installation.innerHTML = 'Yes'
 }
